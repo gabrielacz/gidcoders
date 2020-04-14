d1b9aba9a7733a8fe407c9127a9c321f3bbb8f57
Gabriela <czachor.gabriela@gmail.com>
2020-04-14 15:54:34 +0200

    Amount of topping

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -25,20 +26,37 @@ class Topping(db.Model):
         db.session.commit()
         return entity
 
+
+class PizzaTopping(db.Model):
+    topping_id = db.Column('topping_id', db.Integer, db.ForeignKey('topping.name'), primary_key=True)
+    pizza_id = db.Column('pizza_id', db.Integer, db.ForeignKey('pizza.name'), primary_key=True)
+    amount = db.Column('amount', db.Float, default=1.0)
+
+    @classmethod
+    def get_or_create(cls, topping_id, pizza_id, amount=1.0):
+        entity = PizzaTopping.query.get({'topping_id' : topping_id, 'pizza_id' : pizza_id})
+        if entity:
+            return entity
+        return PizzaTopping.create(topping_id, pizza_id, amount=amount)
+
+    @classmethod
+    def create(cls, topping_id, pizza_id, amount=1.0):
+        entity = cls(
+            topping_id=topping_id,
+            pizza_id=pizza_id,
+            amount=amount
+        )
+        db.session.add(entity)
+        db.session.commit()
+        return entity
+
     def serialize(self):
-        return {"topping": self.name}
-
-
-pizza_topping_identifier = db.Table('pizza_topping_identifier',
-                                    db.Column('topping_id', db.Integer, db.ForeignKey('topping.name')),
-                                    db.Column('pizza_id', db.Integer, db.ForeignKey('pizza.name'))
-                                    )
+        return {"topping": self.topping_id, "amount": self.amount}
 
 
 class Pizza(db.Model):
     name = db.Column(db.String(200), nullable=False, primary_key=True)
     points = db.Column(db.Integer, default=0, nullable=False)
-    toppings = db.relationship("Topping", secondary=pizza_topping_identifier)
 
     def serialize(self):
         return {
@@ -55,7 +73,7 @@ class Pizza(db.Model):
     @classmethod
     def get_or_create(cls, name, toppings=None):
         if not toppings: toppings = []
-        entity = Pizza.query.get(name) #TODO search by toppings
+        entity = Pizza.query.get(name)  # TODO search by toppings
         if entity:
             return entity
         return Pizza.create(name, toppings)
@@ -67,7 +85,9 @@ class Pizza(db.Model):
     @classmethod
     def create(cls, name, toppings=None):
         if not toppings: toppings = []
-        topp = [Topping.get_or_create(name=topping) for topping in toppings]
+        topp = []
+        for topping in toppings:
+            PizzaTopping.get_or_create(topping_id=topping, pizza_id=name, amount=2.0)
         entity = cls(
             name=name,
             points=0,
@@ -76,6 +96,9 @@ class Pizza(db.Model):
         db.session.add(entity)
         db.session.commit()
         return entity
+
+
+Pizza.toppings = relationship("PizzaTopping")
 
 
 @app.route('/list/', methods=['GET'])
